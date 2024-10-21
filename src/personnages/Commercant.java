9d5f6f335e19bca4f40c57de8032b81bcb0e62e1
Thomas Saurel <srt4886a@ensfsi.onmicrosoft.com>
2024-10-21 18:41:58 +0200

    creation de la classe Commercant

---
+++ src/personnages/Commercant.java
@@ -0,0 +1,22 @@
+package personnages;
+
+public class Commercant extends Humain {
+	
+	
+	
+	public Commercant(String nom, int argent) {
+		super(nom, "thé", argent);
+	}
+
+	public int seFaireExtorquer() {
+		parler("J’ai tout perdu! Le monde est trop injuste.");
+		int argent_perdu = getArgent();
+		perdreArgent(argent_perdu);
+		return argent_perdu;
+	}
+	
+	public void recevoir(int argent) {
+		parler(argent + " sous ! Je te remercie généreux donateur!");
+		gagnerArgent(argent);
+	}
+}
--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -19,14 +19,14 @@ public class Humain {
 		this.argent = argent;
 	}
 
-	public void gagnerArgent(int gain) {
+	protected void gagnerArgent(int gain) {
 		argent = getArgent() + gain;
 	}
 
-	public void perdreArgent(int perte) {
+	protected void perdreArgent(int perte) {
 		argent = getArgent() - perte;
 	}
-	
+
 	public void direBonjour() {
 		parler("Bonjour ! Je m'appelle " + getNom() + " et j'aime boire du " + boissonFavorite + ".");
 	}
@@ -34,21 +34,22 @@ public class Humain {
 	public void boire() {
 		parler("Mmmm, un bon verre de " + boissonFavorite + " ! GLOUPS !");
 	}
-	
+
 	public String acheter(String bien, int prix) {
 		if (getArgent() >= prix) {
-			parler("J'ai " + getArgent() + " sous en poche. Je vais pouvoir m'offrir " +  bien + " à " + prix + " sous.");
+			parler("J'ai " + getArgent() + " sous en poche. Je vais pouvoir m'offrir " + bien + " à " + prix
+					+ " sous.");
 			perdreArgent(prix);
 			return bien;
 		} else {
-			parler("Je n'ai plus que " + getArgent() + " sous en poche. Je ne peux même pas m'offrir " +  bien + " à " + prix + " sous.");
+			parler("Je n'ai plus que " + getArgent() + " sous en poche. Je ne peux même pas m'offrir " + bien + " à "
+					+ prix + " sous.");
 			return null;
 		}
 	}
 
-	public void parler(String parole) {
+	protected void parler(String parole) {
 		System.out.println("(" + getNom() + ") - " + parole);
 	}
-
 
 }
