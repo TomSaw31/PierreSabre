810e1448d81e655b291e0585d52b27461783c9cf
Thomas Saurel <srt4886a@ensfsi.onmicrosoft.com>
2024-10-21 19:00:01 +0200

    creation de la classe Ronin

--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -2,6 +2,7 @@ package histoire;
 
 import personnages.Commercant;
 import personnages.Humain;
+import personnages.Ronin;
 import personnages.Yakuza;
 
 public class HistoireTP4 {
@@ -12,16 +13,19 @@ public class HistoireTP4 {
 		prof.boire();
 		prof.acheter("un jeu", 2);
 		prof.acheter("un kimono", 50);
-	
+
 		Commercant marco = new Commercant("Marco", 20);
 		marco.direBonjour();
 		marco.seFaireExtorquer();
 		marco.recevoir(15);
 		marco.boire();
-		
+
 		Yakuza yaku = new Yakuza("Yaku Le Noir", "whisky", 30, "Warsong");
+		yaku.direBonjour();
 		yaku.extorquer(marco);
-		
-		
+
+		Ronin roro = new Ronin("Roro", "shochu", 60);
+		roro.direBonjour();
+		roro.donner(marco);
 	}
 }
---
+++ src/personnages/Ronin.java
@@ -0,0 +1,18 @@
+package personnages;
+
+public class Ronin extends Humain {
+	private int honneur = 1;
+
+	public Ronin(String nom, String boissonFavorite, int argent) {
+		super(nom, boissonFavorite, argent);
+	}
+
+	public void donner(Commercant beneficiaire) {
+		int don = getArgent()/10;
+		parler(beneficiaire.getNom() + " prend ces " + don + " sous.");
+		beneficiaire.parler(don + " sous ! Je te remercie généreux donateur!");
+		perdreArgent(don);
+		beneficiaire.gagnerArgent(don);
+	}
+
+}
