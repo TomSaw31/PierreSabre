25e0fd6b273acaf080de2b5dcdb2db187034930a
Thomas Saurel <srt4886a@ensfsi.onmicrosoft.com>
2024-10-21 19:15:27 +0200

    duels entre Ronin et Yakuza

--- src/personnages/Ronin.java
+++ src/personnages/Ronin.java
@@ -8,11 +8,21 @@ public class Ronin extends Humain {
 	}
 
 	public void donner(Commercant beneficiaire) {
-		int don = getArgent()/10;
+		int don = getArgent() / 10;
 		parler(beneficiaire.getNom() + " prend ces " + don + " sous.");
-		beneficiaire.parler(don + " sous ! Je te remercie généreux donateur!");
+		beneficiaire.recevoir(don);
 		perdreArgent(don);
-		beneficiaire.gagnerArgent(don);
+	}
+
+	public void provoquer(Yakuza adversaire) {
+		parler("Je t'ai retrouvé vermine, tu vas payer pour ce que tu as fait à ce pauvre marchand!");
+		if (honneur * 2 >= adversaire.getReputation()) {
+			parler("Je t’ai eu petit yakusa!");
+			adversaire.perdre();
+		} else {
+			parler("J'ai perdu contre ce yakuza, mon honneur et ma bourse ont en pris un coup.");
+			adversaire.gagner(getArgent());
+		}
 	}
 
 }
--- src/personnages/Yakuza.java
+++ src/personnages/Yakuza.java
@@ -9,6 +9,10 @@ public class Yakuza extends Humain {
 		this.clan = clan;
 	}
 
+	public String getClan() {
+		return clan;
+	}
+
 	public void extorquer(Commercant victime) {
 		parler("Tiens, tiens, ne serait-ce pas un faible marchand qui passe par là ?");
 		parler(victime.getNom() + ", si tu tiens à la vie donne moi ta bourse !");
@@ -16,6 +20,25 @@ public class Yakuza extends Humain {
 		gagnerArgent(gain);
 		parler("J’ai piqué les " + gain + " sous de " + victime.getNom() + " , ce qui me fait " + getArgent()
 				+ " sous dans ma\r\n" + "poche. Hi ! Hi !");
+	}
+
+	public int perdre() {
+		int perte = getArgent();
+		parler("J’ai perdu mon duel et mes " + getArgent() + " sous, snif... J'ai déshonoré le clan de " + getClan());
+		perdreArgent(perte);
+		reputation--;
+		return perte;
+	}
+
+	public void gagner(int gain) {
+		parler("Ce ronin pensait vraiment battre " + getNom() + " du clan de " + getClan()
+				+ "? Je l'ai dépouillé de ses " + gain + " sous.");
+		reputation++;
+		gagnerArgent(gain);
+	}
+
+	public int getReputation() {
+		return reputation;
 	}
 
 }
